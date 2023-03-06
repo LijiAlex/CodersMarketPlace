fe4b360e9cf0cdb9de0551b407209a988b9c023c
LijiAlex <liji.alex@gmail.com>
2023-03-06 13:00:36 +0530

    day2 django

--- arokee_coders/arokee_coders/settings.py
+++ arokee_coders/arokee_coders/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "webpages.apps.WebpagesConfig",
     "djangocms_admin_style",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -55,7 +57,7 @@ ROOT_URLCONF = "arokee_coders.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -119,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "arokee_coders/coders_static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
