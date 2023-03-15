8f8ccd6a83873df5b87b711edfec4a0ee197fa30
LijiAlex <liji.alex@gmail.com>
2023-03-15 13:08:44 +0530

    day 6

--- arokee_coders/coders/urls.py
+++ arokee_coders/coders/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.coders_home, name="coders_home")
+    path('', views.coders_home, name="coders_home"),
+    path('<int:id>', views.coder_detail, name="coder_detail"),
 ]
--- arokee_coders/coders/views.py
+++ arokee_coders/coders/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Coder
 
 # Create your views here.
 def coders_home(request):
-    return render(request, 'coders/home.html')
+    coders = Coder.objects.order_by('id')
+    data = {
+        'coders': coders,
+    }
+    return render(request, 'coders/home.html', data)
+
+def coder_detail(request, id):
+    data = {
+        'coder_id': id,
+    }
+    return render(request, 'coders/coder_detail.html', data)
